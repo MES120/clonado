f4d097ed59511fa0084ae32bc7215e33e026de63
Marisa <carolajulymati@gmail.com>
2023-03-25 11:40:20 -0300

    Aprendiendo a clonar desde Netbeans.

---
+++ clonado/src/clonado/Clonado.java
@@ -0,0 +1,15 @@
+
+package clonado;
+
+/**
+ *
+ * @author Marisa
+ */
+public class Clonado {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Probando clonar un archivo");
+    }
+    
+}
